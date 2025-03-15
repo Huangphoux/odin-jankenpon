b6a8a234d40b13abefbed7c75efa4ace04bc0475
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-03-15 15:21:58 +0700

    Finish the project
    Yeah I don't think the setTimeout function is working as intended for me.
    I haven't tested this game thoroughly tho.
    Wow, the playRound function is not really clean at all.

--- script.js
+++ script.js
@@ -9,21 +9,71 @@ function getComputerChoice() {
     case 1:
       return "paper";
     case 2:
-      return "scissor";
+      return "scissors";
   }
 }
 
 function getHumanChoice() {
-  let choice = prompt("Your turn.\n0 - Rock, 1 - Paper, 2 - Scissor");
+  let choice = prompt("Your turn. Case-INsensitive.");
+  return choice.toLowerCase();
+}
 
-  switch (parseInt(choice)) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissor";
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  //   console.log(`Human: ${humanChoice}`);
+  //   console.log(`Computer: ${computerChoice}`);
+
+  // Rock and paper
+  if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("You lose! Paper beats Rock.");
+    computerScore++;
+    return;
+  }
+  if (computerChoice == "rock" && humanChoice == "paper") {
+    console.log("You win! Paper beats Rock.");
+    humanScore++;
+    return;
+  }
+
+  // Rock and scissors
+  if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("You win! Rock beats Scissors.");
+    humanScore++;
+    return;
+  }
+  if (computerChoice == "rock" && humanChoice == "scissors") {
+    console.log("You lose! Rock beats Scissors.");
+    computerScore++;
+    return;
+  }
+
+  // Paper and scissors
+  if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("You win! Scissors beats Paper.");
+    humanScore++;
+    return;
+  }
+
+  if (computerChoice == "scissors" && humanChoice == "paper") {
+    console.log("You lose! Scissors beats Paper.");
+    computerScore++;
+    return;
+  }
+
+  console.log("Wow! Ties! I guess no one wins then!");
+}
+
+function playGame(round) {
+  for (let index = 0; index < round; index++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    setTimeout("", 5 * 1000);
   }
 }
 
-console.log(getHumanChoice());
+playGame(5);
+console.log(`Human: ${humanScore}`);
+console.log(`Computer: ${computerScore}`);
